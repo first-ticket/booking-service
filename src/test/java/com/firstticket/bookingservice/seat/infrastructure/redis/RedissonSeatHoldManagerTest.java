9325f73a515a7dc73164e547b046151aee6e7ea7
조하은 <123haeun@naver.com>
2026-04-28 12:39:31 +0900

    test: SeatManager 테스트 코드 작성
    - 좌석 선점 성공/실패 테스트
    - 예매 확정 성공/실패 테스트

--- src/test/java/com/firstticket/bookingservice/seat/infrastructure/redis/RedissonSeatHoldManagerTest.java
+++ src/test/java/com/firstticket/bookingservice/seat/infrastructure/redis/RedissonSeatHoldManagerTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Testcontainers
-public class RedissonSeatHoldManagerTest {
+class RedissonSeatHoldManagerTest {
 
     @Container
     static RedisContainer redis = new RedisContainer(
