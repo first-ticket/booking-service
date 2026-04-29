77f083fca703d973ee833eb47bf8c7c4a916d61c
woongcheol Na <32201345@dankook.ac.kr>
2026-04-29 13:03:09 +0900

    feat : 에러코드 수정

--- src/main/java/com/firstticket/bookingservice/booking/domain/exception/BookingErrorCode.java
+++ src/main/java/com/firstticket/bookingservice/booking/domain/exception/BookingErrorCode.java
@@ -18,7 +18,7 @@ public enum BookingErrorCode implements ErrorCode {
 
     EXPIRED_SESSION_TOKEN(HttpStatus.UNAUTHORIZED, "예매 세션 토큰이 만료되었습니다"),
     INVALID_SESSION_TOKEN(HttpStatus.UNAUTHORIZED, "예매 세션 토큰이 유효하지 않습니다"),
-    EMPTY_SESSION_TOKEN(HttpStatus.UNAUTHORIZED, "예매 토큰이 존재하지 않습니다"),
+    EMPTY_SESSION_TOKEN(HttpStatus.UNAUTHORIZED, "예매 세션 토큰이 존재하지 않습니다"),
 
     INVALID_USER_ID(HttpStatus.FORBIDDEN, "토큰의 사용자 정보가 요청자와 일치하지 않습니다"),
     INVALID_PROGRAM_ID(HttpStatus.FORBIDDEN, "토큰의 프로그램 정보가 요청 경로와 일치하지 않습니다"),
