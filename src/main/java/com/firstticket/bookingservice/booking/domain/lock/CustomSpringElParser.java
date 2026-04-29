ddcd3674c9b4d599e5c429a9cdb7b31c2e012b8c
woongcheol Na <32201345@dankook.ac.kr>
2026-04-29 17:58:04 +0900

    feat : 예매 레코드 생성시 동시성 제어 기능 추가

---
+++ src/main/java/com/firstticket/bookingservice/booking/domain/lock/CustomSpringElParser.java
@@ -0,0 +1,18 @@
+package com.firstticket.bookingservice.booking.domain.lock;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class CustomSpringElParser {
+    public static String getDynamicValue(String[] parameterNames, Object[] args, String key) {
+        ExpressionParser parser = new SpelExpressionParser();
+        StandardEvaluationContext context = new StandardEvaluationContext();
+
+        for (int i = 0; i < parameterNames.length; i++) {
+            context.setVariable(parameterNames[i], args[i]);
+        }
+
+        return parser.parseExpression(key).getValue(context, String.class);
+    }
+}
