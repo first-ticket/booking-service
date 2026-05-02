fadd2a0a82904b0e2300a7f280d234ef3042eb4d
woongcheol Na <32201345@dankook.ac.kr>
2026-05-03 07:54:55 +0900

    TODO 주석 추가

--- src/main/java/com/firstticket/bookingservice/booking/infrastructure/client/program_service/ProgramClient.java
+++ src/main/java/com/firstticket/bookingservice/booking/infrastructure/client/program_service/ProgramClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "program-service", path = "/internal/v1/programs", configuration = ProgramClientConfig.class)
 public interface ProgramClient {
 
-    @GetMapping("/{scheduleId}/info")
+    @GetMapping("/{scheduleId}/info") //TODO 추후 api 나오면 실제 경로로 수정할것
     ProgramScheduleResponse validateSchedule(@PathVariable UUID scheduleId);
 }
