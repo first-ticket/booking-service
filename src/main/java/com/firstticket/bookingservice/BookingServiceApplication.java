07db3924430043bcc5d52902a446793fd685e5a7
조하은 <123haeun@naver.com>
2026-05-14 17:39:58 +0900

    fix: @WebMvcTest 테스트 JPA 컨텍스트 충돌 해결을 위한 JpaConfig 분리

--- src/main/java/com/firstticket/bookingservice/BookingServiceApplication.java
+++ src/main/java/com/firstticket/bookingservice/BookingServiceApplication.java
@@ -2,20 +2,10 @@ package com.firstticket.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.firstticket.bookingservice.global.token") //ConfigurationProperties 붙어있는 클래스를 자동으로 찾아 빈으로 등록
-@EntityScan(basePackages = {
-    "com.firstticket.bookingservice",
-    "com.firstticket.common.messaging"
-})
-@EnableJpaRepositories(basePackages = {
-    "com.firstticket.bookingservice",
-    "com.firstticket.common.messaging"
-})
 public class BookingServiceApplication {
 
     public static void main(String[] args) {
